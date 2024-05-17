edfa7cef877e2528a4396d38681e15c6f704fee4
SHIVADVAIT <156700666+SHIVADVAIT@users.noreply.github.com>
2024-05-17 18:44:30 +0530

    new  changesi1 2

--- routes/user.js
+++ routes/user.js
@@ -16,8 +16,14 @@ router.post("/signup", wrapAsync(async(req,res)=>{
     const newUser = new User({email,username});
     const registeredUser = await User.register(newUser,password);
     console.log(registeredUser);
-     req.flash("success","user was registered");
-     res.redirect("/listings");
+    req.login(registeredUser,(err)=>{
+        if(err){
+     next(err); 
+    }
+    req.flash("success","user was registered");
+    res.redirect("/listings");
+    });
+    
     }
     catch(e){
         req.flash("error",e.message);
