c3f0414e29a4f3315e7e27bcdfa1de1828c1b863
SHIVADVAIT <156700666+SHIVADVAIT@users.noreply.github.com>
2024-05-26 14:21:11 +0530

    mapinsertion

---
+++ public/js/map.js
@@ -0,0 +1,6 @@
+	mapboxgl.accessToken ="pk.eyJ1Ijoic2hpdmFuc2hhc2FzIiwiYSI6ImNsd242YTE3YjB3enkybG5mbWZ3MHo1M3YifQ.MCclAkd2lGMF214hphPc1Q";
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+    });
